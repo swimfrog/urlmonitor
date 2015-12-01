4d0fb2cb229e65b9db8e9f861d2a5708255c0f6b
Ben Hawkins <benh@swimfrog.com>
2015-12-01 11:59:38 -0800

    Initial checkin

---
+++ test-urlmonitor.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# Please use an interpreted language to complete the question below,
+# preferences in order of precedence are Python, Ruby and Perl.  A
+# reasonable and well documented answer could take a few hours to
+# produce.  Please stick to core language features (e.g. with the included
+# batteries for Pythonistas).
+#
+# You are responsible for monitoring a web property for changes, the first
+# proof of concept is a simple page monitor.  The requirements are:
+# 1) Log to a file the changed/unchanged state of the URL
+# http://www.oracle.com/index.html.
+# 2) Retry a configurable number of times in case of connection oriented
+# errors.
+# 3) Handle URL content change or unavailability as a program error with a
+# non-zero exit.
+# 4) Any other design decisions are up to the implementer.  Bonus for
+# solid design and extensibility.
+
+exit;
