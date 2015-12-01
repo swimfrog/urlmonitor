bf212b34bd935c27999c5bba6b7d64233584af8b
Ben Hawkins <benh@swimfrog.com>
2015-12-01 12:46:34 -0800

    Add POD usage information and options handling. Initial approach is defined, next is implementation.
    Minor pod formatting updates.

--- test-urlmonitor.pl
+++ test-urlmonitor.pl
@@ -17,4 +17,63 @@
 # 4) Any other design decisions are up to the implementer.  Bonus for
 # solid design and extensibility.
 
+=head1 NAME
+
+test-urlmonitor.pl - Monitors an individual web page for changes
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+my $VERSION = 0.01;
+
+=head1 SYNOPSIS
+
+test-urlmonitor.pl [--help|h] [--man] [--version|V] [--verbose|v] [--mode MODE] [--retry TIMES] [--timeout TIME] [--output FILE] 
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<--mode MODE>    Use the following method to detect changes:
+
+=over 4
+
+=item    B<content>    Use HTTP GET and monitor the content of the response.
+
+=item    B<timestamp>  Use HTTP HEAD and monitor the timestamp of the response to detect changes.
+
+=back
+
+=item B<--retry TIMES>  Retry failed connections up to this many times. Default is 3.
+
+=item B<--output FILE>  Write status changes to FILE. By default, these messages are written to STDOUT unless an output file is specified.
+
+=item B<--timeout TIME> Wait this many seconds for a response from the server before retrying. Default is 10 seconds.
+
+=back
+
+=cut
+
+my $mode = "timestamp";
+my $retry = 3;
+my $timeout = 10;
+
+use Data::Dumper; #--Perl core
+use Getopt::Long; #--Perl core
+use Pod::Usage; #--Perl core
+
+Getopt::Long::Configure ("bundling");
+GetOptions ('help' => sub { pod2usage(1); },
+            'man' => sub { pod2usage(-exitstatus => 0, -verbose => 2); },
+            'version|V' => sub { print "$0: version $VERSION\n"; exit 1; },
+            'verbose|v+' => \$verbose,
+            'mode=s' => \$mode,
+            'retry=i' => \$retry,
+            'outfile=s' => \$outfile,
+            'timeout=i' => \$timeout,
+) or pod2usage(2);
+
 exit;
